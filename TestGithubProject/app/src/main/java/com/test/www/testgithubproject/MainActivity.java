162b4f541fc29c720199e64c931b9b4bdeac7bf5
aili2015 <aili2015@qq.com>
2016-07-11 10:50:45 +0800

    休息下

--- TestGithubProject/app/src/main/java/com/test/www/testgithubproject/MainActivity.java
+++ TestGithubProject/app/src/main/java/com/test/www/testgithubproject/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 事实上山山水水
+        // 完成
     }
 }
